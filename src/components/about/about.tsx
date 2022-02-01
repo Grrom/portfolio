87645696a573647d2ccf648969f2ed4091163b07
Jerome <josejeromelalunio@gmail.com>
2022-02-01 11:56:47 +0800

    change about text

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,17 +1,18 @@
-import "./about.scss"
+import "./about.scss";
 
 export default function About() {
-    return (
-        <main id="about">
-            <span className="title">
-                About Me
-            </span>
-            <p>
-                I'm a <strong>Web and App Developer</strong> from the Philippines,
-                I like coding and contributing to <strong>OpenSource</strong> community projects.
-                I'm open for <strong>Internships, Freelance or OpenSource projects</strong> related to <strong>App Development or Web Development. </strong>
-                I like Playing around with <strong>linux</strong> but I'm not an expert in it, I just like to do it for fun.
-            </p>
-        </main>
-    )
+  return (
+    <main id="about">
+      <span className="title">About Me</span>
+      <p>
+        I'm a <strong>Web and App Developer</strong> from the Philippines. My
+        hobbies include coding, customizing my PC (I use Arch BTW), watching
+        anime and playing video games. I also like playing around with{" "}
+        <strong>linux</strong> and writing my own scripts to improve my worklow.
+        I'm open for{" "}
+        <strong>Internships, Freelance or OpenSource projects</strong> related
+        to <strong>App Development or Web Development. </strong>{" "}
+      </p>
+    </main>
+  );
 }
