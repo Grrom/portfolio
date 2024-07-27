de0788954e19003a0e63751483e0300dc2c6f477
Grrom <josejeromelalunio@gmail.com>
2024-07-27 09:54:35 +0800

    fix: remove cringe intro and make it even more cringe

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -5,10 +5,14 @@ export default function About() {
     <main id="about">
       <span className="title">About Me</span>
       <p>
-        I'm a <strong>Software Engineer</strong> from the Philippines. My
-        hobbies include coding, customizing my PC (I use Arch BTW XD), watching
-        anime and playing video games. I also like playing around with{" "}
-        <strong>linux</strong> and writing my own scripts to improve my worklow.
+        I'm a Software Engineer from the Philippines. My hobbies include coding,
+        customizing my PC, reading books, and writing my own scripts and
+        automations to improve my worklow.
+      </p>
+      <p>
+        I've worked on a number of different projects ranging from websites,
+        apps and APIs. I'm always looking for new opportunities to learn and
+        grow as a developer.
       </p>
     </main>
   );
